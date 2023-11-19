46dec118e52424c76c9d4f1c5548638909422519
shun <shun.syukatsu@gmail.com>
2023-11-20 00:19:39 +0900

    add KadaiThymeleaf(project)

---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,14 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OutputController {
+
+    @GetMapping("/output")
+    public String postOutput() {
+        return "output";
+    }
+
+}
