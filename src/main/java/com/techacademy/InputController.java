8788c030e2e14ddc8415e5eb1b453fd1c6c9e453
shun <shun.syukatsu@gmail.com>
2023-11-20 20:08:05 +0900

    update input.html, output.html, InputController.java, OutputController.java and messages.properties

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,10 +1,7 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
@@ -12,12 +9,6 @@ public class InputController {
     @GetMapping("/input")
     public String getInput() {
         return "input";
-    }
-
-    @PostMapping("/output")
-    public String postForm(@RequestParam("val") String val, Model model) {
-        model.addAttribute("val", val);
-        return "output";
     }
 
 }
